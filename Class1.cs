89693d143e7adb4b3348b116f51d31a535bb55d3
111 <111>
2024-08-14 01:24:48 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTetris
+{
+    internal class Game // internal может поредачить?
+    {
+        // бе бе бе 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTetris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // -это сообщество помощи алкоголикам? 
+            // - да
+            // - помогите, на бутылку на хватает
+        }
+    }
+}
