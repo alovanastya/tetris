98ac823abb8a618da94c5645d88f12245872c4df
VovaIuk <gubob2015@gmail.com>
2024-08-14 20:04:55 +0300

    Added class Block, Square, State

---
+++ Block.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTetris
+{
+    struct Square
+    {
+        public Square(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+        public int i;
+        public int j;
+    }
+
+    class Block
+    {
+        List<Square> coordinates;
+        public List<Square> moveToRight() {
+            return null;
+        }
+    }
+}
---
+++ State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTetris
+{
+    class State
+    {
+        Block activeBlock;
+        int[,] gameBoard = new int[20, 10];
+    }
+}
